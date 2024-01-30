0b67d35ef42e8d4232fd6217c56adeb776197113
Bernardo Subercaseaux <bernardosubercaseaux@gmail.com>
2024-01-30 14:05:15 -0500

    hot changes from meeting

--- Lean/Geo/Main.lean
+++ Lean/Geo/Main.lean
@@ -2,17 +2,69 @@ import Geo.PlaneGeometry
 import Geo.SymmetryBreaking
 import Geo.Slope
 import Geo.Point
+import Mathlib.Data.Finset.Basic
 
-structure EmptyCovexHexagon (a b c d e f: Point) : Prop :=
+noncomputable section
+
+structure EmptyCovexHexagon (S: Finset Point) (pts: Finset Point) : Prop :=
   --
+  hexagon: S.card = 6
+  sorry
 
 
-theorem EmptyHexagonTheorem (pts: List Point)
-  (h: List.length pts ≥ 30) :
-  ∃ (a b c d e f: Fin (List.length pts)),
-  EmptyCovexHexagon (pts.get a)
-                    (pts.get b)
-                    (pts.get c)
-                    (pts.get d)
-                    (pts.get e)
-                    (pts.get f) := by sorry
+theorem EmptyHexagonTheorem (pts: Finset Point)
+    (h: pts.card ≥ 30) :
+  ∃ (S : Finset Point),
+    (S ⊆ pts) ∧ EmptyCovexHexagon S pts := by sorry
+
+
+structure Triangle (S: Finset Point) : Prop :=
+  --
+  triangle: S.card = 3
+  triangle_list : S.toList.length = 3
+  no_three_collinear: σ (S.toList.get ⟨0, by linarith⟩) (S.toList.get ⟨1, by linarith⟩) (S.toList.get ⟨2, by linarith⟩) ≠ Orientation.Collinear
+
+def pt_in_triangle (p: Point) (T: Triangle S): Prop :=
+  let L_S := S.toList
+  have h: L_S.length = 3 := T.triangle_list
+  -- let sorted_L_S := L_S.sort (λ p1 p2 => p1.x < p2.x)
+  let a := L_S.get ⟨0, by linarith⟩
+  let b := L_S.get ⟨1, by linarith⟩
+  let c := L_S.get ⟨2, by linarith⟩
+  sorry
+
+
+
+def finset_sort (S : Finset Point) : List Point :=
+  let L_S := S.toList
+  let A_S := L_S.toArray
+  (A_S.insertionSort (λ p1 p2 => p1.x <= p2.x)).toList
+
+theorem finset_sort_sorts (S : Finset Point) : (IsSortedPts (finset_sort S)) := by
+  sorry
+
+structure ListEmptyTriangle (L : List Point) (pts : List Point) : Prop :=
+  --
+  let sorted_L :=
+
+
+structure EmptyTriangle (S: Finset Point) (pts: Finset Point) : Prop :=
+  --
+  h : ListEmptyTriangle S.toList pts.toList
+  -- triangle: Triangle S
+  -- empty: ∀ p, p ∈ pts ∧ p ∉ S → ¬ (pt_in_triangle p triangle)
+
+
+
+
+
+open Classical
+structure GeneralPosition (pts: Finset Point) : Prop :=
+  no_three_collinear: ∀ p1 p2 p3: Point, {p1, p2, p3} ⊆ pts
+                        → σ p1 p2 p3 ≠ Orientation.Collinear
+
+theorem EmptyTriangle10Theorem (pts: Finset Point)
+    (gp : GeneralPosition pts)
+    (h: pts.card = 10) :
+  ∃ (S : Finset Point),
+    S ⊆ pts ∧ EmptyTriangle S pts := by sorry
--- Lean/Geo/Point.lean
+++ Lean/Geo/Point.lean
@@ -5,6 +5,7 @@ import Mathlib.Algebra.Algebra.Basic
 structure Point where
   x : Real
   y : Real
+-- deriving DecidableEq
 
 def determinant_pts (a b c : Point) : Real :=
   a.x * b.y + b.x * c.y + c.x * a.y - a.y * b.x - b.y * c.x - c.y * a.x
