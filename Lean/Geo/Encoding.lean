acb49cbaac4c0967d60b9bd4fb75af8d17f8456e
James Gallicchio <jamesgallicchio@gmail.com>
2024-02-05 18:42:23 -0500

    use new tseitin encoding for readability

--- Lean/Geo/Encoding.lean
+++ Lean/Geo/Encoding.lean
@@ -88,9 +88,9 @@ def Array.finRange (n : Nat) : Array (Fin n) :=
 def trianglessss {n : Nat} (hn : n ≥ 3) : VEncCNF (Literal (Var n)) Unit ⊤ :=
   for_all (Array.finRange n) (fun a =>
     for_all (Array.finRange n) (fun b =>
-      guard (a < b) (fun hab =>
+      VEncCNF.guard (a < b) (fun hab =>
         for_all (Array.finRange n) (fun c =>
-          guard (b < c) (fun hbc =>
+          VEncCNF.guard (b < c) (fun hbc =>
             -- Cayden notes: For whatever reason, "let" notation isn't allowed in these funs
             --let sabc := LitVar.mkPos <| Var.sigma a b c hab hbc
             --let habc := LitVar.mkPos <| Var.hole  a b c hab hbc
@@ -98,26 +98,23 @@ def trianglessss {n : Nat} (hn : n ≥ 3) : VEncCNF (Literal (Var n)) Unit ⊤ :
 
             -- Signotope axioms
             ( for_all (Array.finRange n) (fun d =>
-                guard (c < d) (fun hcd =>
-                  seq
-                  ( andImply (#[
-                      LitVar.mkPos <| Var.sigma a b c hab hbc,
-                      LitVar.mkPos <| Var.sigma a c d (lt_trans hab hbc) hcd ])
-                    (LitVar.mkPos <| Var.sigma a b d hab (lt_trans hbc hcd)) )
-                  ( seq
-                  ( andImply (#[
-                      LitVar.mkPos <| Var.sigma a b c hab hbc,
-                      LitVar.mkPos <| Var.sigma b c d hbc hcd ])
-                    (LitVar.mkPos <| Var.sigma a c d (lt_trans hab hbc) hcd) )
-                  ( seq
-                  ( andImply (#[
-                      LitVar.mkNeg <| Var.sigma a b c hab hbc,
-                      LitVar.mkNeg <| Var.sigma a c d (lt_trans hab hbc) hcd ])
-                    (LitVar.mkNeg <| Var.sigma a b d hab (lt_trans hbc hcd)) )
-                  ( andImply (#[
-                      LitVar.mkNeg <| Var.sigma a b c hab hbc,
-                      LitVar.mkNeg <| Var.sigma b c d hbc hcd ])
-                    (LitVar.mkNeg <| Var.sigma a c d (lt_trans hab hbc) hcd) ) ) )
+                VEncCNF.guard (c < d) (fun hcd =>
+                  tseitin
+                    (.conj
+                      (.impl (.conj (Var.sigma a b c hab hbc)
+                                    (Var.sigma a c d (lt_trans hab hbc) hcd))
+                             (Var.sigma a b d hab (lt_trans hbc hcd)) )
+                    (.conj
+                      (.impl (.conj (Var.sigma a b c hab hbc) (Var.sigma b c d hbc hcd))
+                             (.var <| Var.sigma a c d (lt_trans hab hbc) hcd) )
+                    (.conj
+                      (.impl (.conj (.neg <| Var.sigma a b c hab hbc)
+                                    (.neg <| Var.sigma b c d hbc hcd))
+                             (.neg <| Var.sigma a b d hab (lt_trans hbc hcd)) )
+                      (.impl (.conj (.neg <| Var.sigma a b c hab hbc)
+                                    (.neg <| Var.sigma b c d hbc hcd))
+                             (.neg <| Var.sigma a c d (lt_trans hab hbc) hcd) )
+                    ) ) )
                 )
               )
             )
@@ -130,21 +127,19 @@ def trianglessss {n : Nat} (hn : n ≥ 3) : VEncCNF (Literal (Var n)) Unit ⊤ :
                   guard (a < x ∧ x < b) (fun haxb =>
                     -- Ix → ¬ Habc
                     -- Cayden asks: Have a simple .impl operation?
-                    addClause (#[
-                      LitVar.mkNeg <| Var.inside x a b c hab hbc haxb.1 (lt_trans haxb.2 hbc),
-                      LitVar.mkNeg <| Var.hole a b c hab hbc ] )
+                    imply (LitVar.mkNeg <| Var.inside x a b c hab hbc haxb.1 (lt_trans haxb.2 hbc))
+                          (LitVar.mkNeg <| Var.hole a b c hab hbc)
                   )
                 )
               )
 
               -- b < x < c
               ( for_all (Array.finRange n) (fun x =>
-                  guard (b < x ∧ x < c) (fun hbxc =>
+                  VEncCNF.guard (b < x ∧ x < c) (fun hbxc =>
                     -- Ix → ¬ Habc
                     -- Cayden asks: Have a simple .impl operation?
-                    addClause (#[
-                      LitVar.mkNeg <| Var.inside x a b c hab hbc (lt_trans hab hbxc.1) hbxc.2,
-                      LitVar.mkNeg <| Var.hole a b c hab hbc ] )
+                    imply (LitVar.mkPos <| Var.inside x a b c hab hbc (lt_trans hab hbxc.1) hbxc.2)
+                          (LitVar.mkNeg <| Var.hole a b c hab hbc)
                   )
                 )
               )
@@ -157,4 +152,5 @@ def trianglessss {n : Nat} (hn : n ≥ 3) : VEncCNF (Literal (Var n)) Unit ⊤ :
       )
     )
   )
-  |> mapProp sorry
+  |> mapProp (by
+    sorry)
