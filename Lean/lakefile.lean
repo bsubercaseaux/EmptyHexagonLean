c4eac4cf786b389e586280792ce5087265493586
James <jamesgallicchio@gmail.com>
2024-02-13 12:08:05 -0500

    update to fix first round of perf bugs

--- Lean/lakefile.lean
+++ Lean/lakefile.lean
@@ -10,7 +10,7 @@ package «geo» where
   -- add any additional package configuration options here
 
 require «lean-sat» from git
-  "https://github.com/JamesGallicchio/LeanSAT.git" @ "enccnf-array"
+  "https://github.com/JamesGallicchio/LeanSAT.git" @ "dev"
 
 @[default_target]
 lean_lib «Geo» where
