5e2753fb095a5b8677f77370012b6386400da0ef
Cayden Codel <ccodel@andrew.cmu.edu>
2024-02-15 16:59:20 -0500

    Affine transforms preserve sigma equivalence

--- Lean/Geo/PointsToWB/TMatrix.lean
+++ Lean/Geo/PointsToWB/TMatrix.lean
@@ -17,6 +17,11 @@ def pt_transform (M : Matrix (Fin 3) (Fin 3) Real) (p : Point) : Point :=
   let A := M * (pt_to_vec p)
   vec_to_pt A
 
+-- CC: Since we unfold a lot of definitions, having a computation can help
+theorem pt_transform_eq (M : Matrix (Fin 3) (Fin 3) Real) (p : Point) :
+    pt_transform M p = ![M 0 0 * p.x + M 0 1 * p.y + M 0 2, M 1 0 * p.x + M 1 1 * p.y + M 1 2] := by
+  simp [pt_transform, pt_to_vec, vec_to_pt, Matrix.mul_apply, Finset.univ, Fintype.elems, List.finRange, add_assoc]
+  rfl
 
 /-- `M` is an affine transformation matrix. -/
 structure TMatrix (M : Matrix (Fin 3) (Fin 3) Real) : Prop :=
@@ -28,31 +33,31 @@ namespace TMatrix
 def toLinearMatrix (T : TMatrix M) : Matrix (Fin 2) (Fin 2) Real :=
   !![M 0 0, M 0 1 ; M 1 0 , M 1 1]
 
-theorem det_eq_det_toLinearMatrix (T : TMatrix M)
-  : Matrix.det M = Matrix.det T.toLinearMatrix := by
-  -- prove by unfolding everything
-  sorry
+theorem det_eq_det_toLinearMatrix (T : TMatrix M) : M.det = T.toLinearMatrix.det := by
+  rw [toLinearMatrix, Matrix.det_fin_three]
+  have htr := T.third_row
+  have : M ⟨2, by trivial⟩ = M 2 := rfl
+  rw [this] at htr
+  simp [htr]
 
 noncomputable def toAffineMap (T : TMatrix M) : Point →ᵃ[ℝ] Point where
   toFun := pt_transform M
   linear := Matrix.toEuclideanLin T.toLinearMatrix
   map_vadd' := by
     intro p1 p2
-    -- godawful proof we're so sorry
-    simp [toLinearMatrix, pt_transform, Matrix.toEuclideanLin,
-      Matrix.vecTail, Matrix.vecHead, Point.x, Point.y]
-    simp [vec_to_pt, pt_to_vec, Matrix.mul_apply]
-    simp [Finset.univ, Fintype.elems, List.finRange]
-    simp [WithLp.equiv, Equiv.refl]
-    ext <;> simp [Point.x, Point.y] <;> ring
+    simp_rw [vadd_eq_add, pt_transform_eq]
+    simp [toLinearMatrix, Matrix.toEuclideanLin_eq_toLin]
+    show _ = Matrix.mulVec !![M 0 0, M 0 1 ; M 1 0 , M 1 1] p2 + _
+    simp [Matrix.mulVec, Matrix.vecHead, Matrix.vecTail, mul_add]
+    ring_nf
 
 theorem pts_to_matrix_pt_transform (p q r : Point) {M : Matrix (Fin 3) (Fin 3) Real} (T : TMatrix M) :
     pts_to_matrix (pt_transform M p) (pt_transform M q) (pt_transform M r) = M * pts_to_matrix p q r := by
-  unfold pts_to_matrix
+  simp_rw [pt_transform_eq, pts_to_matrix]
   ext i j
   fin_cases i
-  <;> simp [Matrix.mul_apply, pt_transform, vec_to_pt, pt_to_vec, T.third_row, Finset.univ, Fintype.elems, List.finRange]
-  <;> fin_cases j <;> simp
+  <;> simp [Matrix.mul_apply, Finset.univ, Fintype.elems, List.finRange, T.third_row]
+  <;> fin_cases j <;> simp [add_assoc] <;> rfl
 
 theorem pt_transform_preserves_sigma (p q r : Point) (T : TMatrix M) :
     σ (pt_transform M p) (pt_transform M q) (pt_transform M r) = σ p q r := by
@@ -72,38 +77,13 @@ theorem toEquivσ (S: Set Point) (T: TMatrix M) :
   f := by
     let eqv := LinearMap.equivOfDetNeZero T.toAffineMap.linear (by
       have := T.det_pos
-      simp [toAffineMap]
-      stop
-      aesop)
-    have := eqv.injective
-    simp [LinearMap.equivOfDetNeZero, LinearEquiv.ofIsUnitDet] at this
-    clear eqv
-    stop
-    exact Equiv.Set.image (⇑f) S this
+      rw [det_eq_det_toLinearMatrix] at this
+      simp [toAffineMap, Matrix.toEuclideanLin_eq_toLin]
+      exact ne_of_gt this)
+    replace eqv := eqv.injective
+    simp [LinearMap.equivOfDetNeZero, LinearEquiv.ofIsUnitDet] at eqv
+    exact Equiv.Set.image _ S eqv
 
   hσ := by
-    stop
-    set resulting_pts := transform_points pts M
-    have same_length : pts.length = resulting_pts.length := by
-      simp
-      unfold transform_points
-      simp [List.map]
-
-    have same_orientation : ∀ {i j k} (hi : i < pts.length) (hj : j < pts.length) (hk : k < pts.length),
-      σ (pts.get ⟨i, hi⟩) (pts.get ⟨j, hj⟩) (pts.get ⟨k, hk⟩) =
-      σ (resulting_pts.get ⟨i, by rw [←same_length] ; exact hi⟩)
-                    (resulting_pts.get ⟨j, by rw [←same_length] ; exact hj⟩)
-                    (resulting_pts.get ⟨k, by rw [←same_length] ; exact hk⟩) := by
-        intros i j k hi hj hk
-        have ti : pt_transform (pts.get ⟨i, hi⟩) M = resulting_pts.get ⟨i, by rw [←same_length] ; exact hi⟩ := by
-          simp [transform_points]
-
-        have tj : pt_transform (pts.get ⟨j, hj⟩) M = resulting_pts.get ⟨j, by rw [←same_length] ; exact hj⟩ := by
-          simp [transform_points]
-        have tk : pt_transform (pts.get ⟨k, hk⟩) M = resulting_pts.get ⟨k, by rw [←same_length] ; exact hk⟩ := by
-          simp [transform_points]
-
-        rw [←ti, ←tj, ←tk]
-        rw [transform_preserve_sigma]
-        exact T
-    exact ⟨same_length, same_orientation⟩
+    intro p q r
+    simp [pt_transform_preserves_sigma p q r T]
