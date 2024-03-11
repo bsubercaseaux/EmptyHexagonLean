cdf3b2def2ff5bfc4539bd7286920c108d3ba928
Mario Carneiro <di.gama@gmail.com>
2024-03-10 21:48:13 -0400

    import the target theorems

--- Lean/Geo.lean
+++ Lean/Geo.lean
@@ -1,2 +1,4 @@
 -- This module serves as the root of the `Geo` library.
 -- Import modules here that should be built as part of the library.
+import Geo.Triangle.TheMainTheoremNew
+import Geo.Hexagon.TheMainTheoremNew
--- Lean/lakefile.lean
+++ Lean/lakefile.lean
@@ -14,8 +14,6 @@ require «lean-sat» from git
 
 @[default_target]
 lean_lib «Geo» where
-  -- FIXME: this compiles all the exploratory stuff, remove it once the proof is done
-  globs := #[.andSubmodules `Geo]
 
 lean_exe run_geo where
   root := `Geo
