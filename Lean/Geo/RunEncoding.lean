9ee5b929a5bc25e69b86e480dff41d2bfd46ac5c
Mario Carneiro <di.gama@gmail.com>
2024-03-14 01:20:07 -0400

    generate the same variable names as the C code

--- Lean/Geo/RunEncoding.lean
+++ Lean/Geo/RunEncoding.lean
@@ -27,5 +27,35 @@ def main (args : List String) : IO Unit := do
       let h ← IO.FS.Handle.mk out IO.FS.Mode.write
       for v in vars do
         h.putStrLn v.toCode
-    Solver.Dimacs.printFormula (IO.print) enc
+      Solver.Dimacs.printFormula IO.print enc
+    else
+      let r4 (a b c d : Nat) : Nat :=
+        a + (b-2)*(b-1)/2 + (c-3)*(c-2)*(c-1)/6 + (d-4)*(d-3)*(d-2)*(d-1)/24
+      let r3 a b c := r4 a b c 0
+      let inside (x a b c : Nat) :=
+        let (l, r) := if x > b then (b, x) else (x, b)
+        r3 0 0 (n+1) + 2 * r4 a l r c - (if x < b then 1 else 0)
+      let hole a b c :=
+        r4 0 0 (n+1) (n+1) + r4 a b c (n+1)
+      let nVars :=
+        r3 0 0 (n+1) -- base variables
+        + 2 * r4 0 0 0 (n+1) -- inside variables
+        + r3 0 0 (n+1)  -- hole variables
+        + 3 * r3 0 0 (n+1)
+      let aux := 1 + 2 * r4 0 0 (n+1) (n+1)
+      let def_ a b c d := aux + 3 * r3 a b c + d
+      let toNat
+      | .sigma a b c => r3 (a+2) (b+2) (c+2)
+      | .inside x a b c => inside (x+2) (a+2) (b+2) (c+2)
+      | .hole₀ a b c => hole (a+1) (b+2) (c+2)
+      | .cap a b c => def_ (a+2) (b+2) (c+2) 0
+      | .cup a b c => def_ (a+2) (b+2) (c+2) 1
+      | .capF a b c => def_ (a+2) (b+2) (c+2) 2
+      let m (v : Nat) : Nat :=
+        match vars[v]? with
+        | none => v - vars.size + nVars
+        | some var => toNat var
+      let enc : ICnf := enc.map <| Array.map fun l =>
+        LitVar.mkLit ILit (m (LitVar.toVar l).natPred - 1).succPNat (LitVar.polarity l)
+      Solver.Dimacs.printFormula IO.print enc
   | _ => help
