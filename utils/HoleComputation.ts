a621a79395eb52aef11225a7278a8f12876b94e6
Mario Carneiro <di.gama@gmail.com>
2024-03-22 05:49:06 -0400

    simplify HoleComputation.ts some more

--- utils/HoleComputation.ts
+++ utils/HoleComputation.ts
@@ -71,13 +71,12 @@ for (let idP = 0; idP < allPoints.length; idP++) {
         proceed(Q_i[0], j);
         Q_i.shift();
       }
-      edges.push([i, j]);
+      // discard edges including point p.
+      if (i != 0) edges.push([i, j]);
       Q_j.push(i);
     }
     proceed(i, i + 1);
   }
-  // discard edges including point p.
-  edges = edges.filter(edge => edge[0] !== 0 && edge[1] !== 0);
 
   // maxChain
   let Lmap: { [index: string]: number } = {};
@@ -99,58 +98,13 @@ for (let idP = 0; idP < allPoints.length; idP++) {
         if (Lmap[String(pOutgoing[l])] > m) {
           m = Lmap[String(pOutgoing[l])];
           Lmap[String(pIncoming[j])] = m + 1;
+          if (m >= r - 3) {
+            console.log("Found empty convex polygon of desired length!!");
+          }
         }
         l -= 1;
       }
     }
   }
-  console.log(Lmap)
-
-  // chainTreatment
-  let ChMap: { [index: string]: number[][][] } = {};
-  for (let i = 0; i < pointSequence.length - 1; ++i) {
-    let S_i = edges.filter(edge => edge[1] === i);
-    let S_o = edges.filter(edge => edge[0] === i);
-    let S_o_sorted = S_o.sort((edge1, edge2) => Lmap[String(edge2)] - Lmap[String(edge1)]);
-    for (let j = 0; j < S_o.length; ++j) {
-      //console.log("S_o[j]", S_o[j], "Lmap[S_o[j]]", Lmap[S_o[j]], "r-2", r-2);
-      if (Lmap[String(S_o[j])] >= r - 2) {
-        // console.log("value is >= r-2", (r-2));
-        ChMap[String(S_o[j])] = [[S_o[j]]];
-      } else {
-        ChMap[String(S_o[j])] = [];
-      }
-    }
-
-    let m = 0;
-    let om = S_o.length - 1;
-    for (let j = 0; j < S_i.length; ++j) {
-      while (m <= S_o.length - 1) {
-        let ccw_jm = ccw(
-          pointSequence[S_i[j][0]],
-          pointSequence[S_i[j][1]],
-          pointSequence[S_o[m][1]]);
-        if (ccw_jm) {
-          break;
-        }
-        S_o_sorted = S_o_sorted.filter(edge => edge !== S_o[m]);
-        om -= 1;
-        m += 1;
-      }
-      ChMap[String(S_i[j])].forEach(chain => {
-        let t = 0;
-        let l = chain.length;
-        while (t <= om && Lmap[String(S_o_sorted[t])] >= r - 2 - l) {
-          chain.push(S_o_sorted[t]);
-          if (l === r - 3) {
-            console.log("Found empty convex polygon of desired length!!");
-            console.log("l", l, " chain length: ", chain.length, " chain: ", chain);
-          } else {
-            ChMap[String(S_o_sorted[t])].push(chain);
-          }
-          t += 1;
-        }
-      });
-    }
-  }
+  // console.log(Lmap)
 }
